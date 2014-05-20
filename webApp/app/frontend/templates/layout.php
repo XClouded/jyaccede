9791a447a7050e5bf18bb97c80abdce7a7e29d8a
Lelexxx <rruiz.alex@gmail.com>
2014-05-20 23:19:23 +0200

    Add bootstrap files. Add sql files.

--- webApp/app/frontend/templates/layout.php
+++ webApp/app/frontend/templates/layout.php
@@ -1,10 +1,15 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>VisitMyCity, Welcome !</title>
+            <title>VisitMyCity, Welcome !</title>
+
+            <!-- BOOTSTRAP -->
+            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+            <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 	</head>
 	
 	<body>
-		<?= $templateEngine->renderBody($templateBody, $parameters); ?>
+            <?= $templateEngine->renderBody($templateBody, $parameters); ?>
 	</body>
 </html>
