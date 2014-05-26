04abc61d5486a49968e9221e58f0801d899c57e2
Charruel Maxime <m.charruel@overscan.fr>
2014-05-26 13:15:38 +0200

    Push modif

--- mobileApp/VisitMyCityAndroid/app/src/main/java/visitmycityandroid/activity/VisitMyCityActivity.java
+++ mobileApp/VisitMyCityAndroid/app/src/main/java/visitmycityandroid/activity/VisitMyCityActivity.java
@@ -11,7 +11,6 @@ public abstract class VisitMyCityActivity extends Activity{
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
