a0083b2cd670999ec263b5c8c5f5e0ae6e41ccef
Ruiz Alexandre <rruiz.alex@gmail.com>
2014-06-23 15:08:59 +0200

    Remove unused files

--- mobileApp/VisitMyCityAndroid/app/src/main/java/visitmycityandroid/interfaces/CanalTpTaskListener.java
+++
@@ -1,9 +0,0 @@
-package visitmycityandroid.interfaces;
-
-import java.util.ArrayList;
-
-import visitmycityandroid.model.LineModel;
-
-public interface CanalTpTaskListener {
-    public abstract void OnCompleted(final ArrayList<LineModel> lines);
-}
