7f03d4d6fe20be7da6a442c4e58113c0ce47a6ac
Charruel Maxime <m.charruel@overscan.fr>
2014-07-01 17:24:09 +0200

    [ADD] allow user to update lines and stop points on frontend

--- webApp/src/JyAccede/Bundle/APIBundle/Controller/FrontController.php
+++ webApp/src/JyAccede/Bundle/APIBundle/Controller/FrontController.php
@@ -6,16 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FrontController extends Controller
 {
-    public function listAction()
+    public function listAction(Request $request)
     {
+        $isPmr = $request->request->get('isPmr');
+        
         $stopPointsRepo=$this->getDoctrine()->getRepository("JyAccedeAPIBundle:StopPoint");
         $linesRepo=$this->getDoctrine()->getRepository("JyAccedeAPIBundle:Line");
 
-        $stopPoints=$stopPointsRepo->findBy(array("isPmr"=>"1"),array("name"=>"asc"));
-        $lines=$linesRepo->findBy(array("isPmr"=>"1"),array("name"=>"asc"));
-
-        $objects=array_merge($stopPoints,$lines);
+        $stopPoints=$stopPointsRepo->findBy(array("isPmr"=>$isPmr),array("name"=>"asc"));
+        $lines=$linesRepo->findBy(array("isPmr"=>$isPmr),array("name"=>"asc"));
 
         return $this->render('JyAccedeAPIBundle:Front:list.html.twig', array('lines' => $lines,"stops"=>$stopPoints));
+    }
+    
+    public function updateAction(Request $request){
+        if($request->isMethod("POST")){  
+            $id = $request->request->get('id');
+            if($request->request->get("type") == 'line'){
+                $stopPointsRepo = $this->getDoctrine()->getRepository("JyAccedeAPIBundle:StopPoint");
+                $entity = $stopPointsRepo->findOneBy(array("id"=>$id));
+            }
+            elseif($request->request->get("type") == 'stopPoint'){
+                $linesRepo = $this->getDoctrine()->getRepository("JyAccedeAPIBundle:Line");
+                $entity = $linesRepo->findOneBy(array("id"=>$id));
+            }          
+        }
+        
+        $this->redirect('/arrets_lignes');
     }
 }
